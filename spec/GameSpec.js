5f62d4954f48380211a515a92ec3ade8fcfca218
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-04 20:14:25 +0000

    #calcScores calculates the score of complete frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,26 +1,37 @@
 var Game = require('../src/Game.js')
 
-describe('Game', function() {
+describe('Game', function () {
   var game
-  beforeEach(function() {
+  beforeEach(function () {
     game = new Game()
-  });
+  })
 
-  describe('#addRollToFrame', function() {
-    it('sets a roll in the current frame', function() {
+  describe('#addRollToFrame', function () {
+    it('sets a roll in the current frame', function () {
       game.addRollToFrame(4)
 
-      expect(game.frames[0].rollOne).toEqual(4);
-    });
+      expect(game.frames[0].rollOne).toEqual(4)
+    })
 
-    it('moves on to the next frame when current frame is complete', function() {
+    it('moves on to the next frame when current frame is complete', function () {
       game.addRollToFrame(4)
       game.addRollToFrame(4)
       game.addRollToFrame(4)
 
-      expect(game.frames[0].rollOne).toEqual(4);
-      expect(game.frames[0].rollTwo).toEqual(4);
-      expect(game.frames[1].rollOne).toEqual(4);
-    });
-  });
-});
+      expect(game.frames[0].rollOne).toEqual(4)
+      expect(game.frames[0].rollTwo).toEqual(4)
+      expect(game.frames[1].rollOne).toEqual(4)
+    })
+  })
+
+  describe('#calcscores', function () {
+    it('calculates the score of complete frames', function () {
+      game.addRollToFrame(4)
+      game.addRollToFrame(4)
+
+      game.calcScores()
+
+      expect(game.frames[0].score).toEqual(8)
+    })
+  })
+})
--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,29 @@
 var Frame = require('./Frame.js')
 
-let i = 0
-
 function Game(frames = [new Frame, new Frame, new Frame, new Frame, new Frame,
   new Frame, new Frame, new Frame, new Frame, new Frame, ])
 {
   this.frames = frames
+  this.i = 0
+  this.current_frame = this.frames[this.i]
 }
 
 Game.prototype.addRollToFrame = function (num) {
-  var current_frame = this.frames[i]
-  if (current_frame.rollTwo !== undefined) {
-    i++
-    current_frame = this.frames[i]
+  var current_frame = this.frames[this.i]
+  if (this.current_frame.isFinishedRolling()) {
+    this.i++
+    this.current_frame = this.frames[this.i]
   }
-  current_frame.addRoll(num)
+  this.current_frame.addRoll(num)
 };
 
+Game.prototype.calcScores = function () {
+  for (var i = 0; i < this.frames.length; i++) {
+    if (this.frames[i].rollTwo === undefined) {
+      continue
+    }
+    this.frames[i].calculateScore()
+  }
+};
 
 module.exports = Game
