ad6e5171b348326e8cc5bd3a02852d0718190de1
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-04 18:57:35 +0000

    removed test doubles
    this test will serve as an integration test of 10 frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,29 +1,26 @@
 var Game = require('../src/Game.js')
 
 describe('Game', function() {
-  var frame1
-  var frame2
-
   var game
+  beforeEach(function() {
+    game = new Game()
+  });
 
   describe('#addRollToFrame', function() {
     it('sets a roll in the current frame', function() {
-      frame1 = jasmine.createSpyObj('frame1', ['addRoll']);
-      frames = [frame1]
-      game = new Game(frames)
       game.addRollToFrame(4)
-      expect(frame1.addRoll).toHaveBeenCalledWith(4);
+
+      expect(game.frames[0].rollOne).toEqual(4);
     });
 
     it('moves on to the next frame when current frame is complete', function() {
-      frame1 = jasmine.createSpyObj('frame1', ['addRoll', 'rollTwo']);
-      frame2 = jasmine.createSpyObj('frame2', ['addRoll']);
-      frames = [frame1, frame2]
-      game = new Game(frames)
-
       game.addRollToFrame(4)
-      expect(frame2.addRoll).toHaveBeenCalledWith(4);
+      game.addRollToFrame(4)
+      game.addRollToFrame(4)
+
+      expect(game.frames[0].rollOne).toEqual(4);
+      expect(game.frames[0].rollTwo).toEqual(4);
+      expect(game.frames[1].rollOne).toEqual(4);
     });
   });
-
 });
