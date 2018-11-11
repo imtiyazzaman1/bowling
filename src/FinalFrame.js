db8738313db091e5b961173ce2187ea69a4889df
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-11 11:38:29 +0000

    refactored

--- src/FinalFrame.js
+++ src/FinalFrame.js
@@ -1,4 +1,4 @@
-function FinalFrame() {
+function FinalFrame () {
   this.bonus = []
   this.rollThree = undefined
 }
@@ -9,41 +9,45 @@ FinalFrame.prototype.addRoll = function (num) {
   this._checkIfComplete()
 
   if (this.rollOne === undefined) {
-    this._checkNumber(num)
-    this._checkIfStrike(num)
-    this.rollOne = num
-
+    this._setRollOne(num)
   } else if (this.rollTwo === undefined) {
-    this._checkRemainingPins(num)
-    this.rollTwo = num
-    this._checkIfSpare()
-
-    if (!this.isSpare && !this.isStrike) {
-      this._setAsComplete()
-    }
-  } else if (this.rollThree === undefined && !this.isComplete) {
-    if (this.isSpare || this.isStrike) {
-      this.bonus.push(num)
-    }
+    this._setRollTwo(num)
+  } else if (this._isBonus()) {
     this._setRollThree(num)
-    this._setAsComplete()
   }
-};
+}
+
+FinalFrame.prototype._setAsStrike = function () {
+  this.isStrike = true
+}
 
 FinalFrame.prototype._checkIfStrike = function (num) {
   if (num === 10) {
     this.isStrike = true
   }
-};
+}
 
 FinalFrame.prototype._checkRemainingPins = function (num) {
-  if (this.rollOne !== 10) {
-    this._checkNumber(num)
-  }
-};
+  if (this.rollOne !== 10) this._checkNumber(num)
+}
+
+FinalFrame.prototype._setRollTwo = function (num) {
+  this._checkRemainingPins(num)
+  this.rollTwo = num
+  this._checkIfSpare()
+  this._checkIfBonus()
+}
 
 FinalFrame.prototype._setRollThree = function (num) {
-  if (this.isSpare || this.isStrike && this.rollTwo !== undefined) {
-    this.rollThree = num
-  }
-};
+  this.bonus.push(num)
+  this.rollThree = num
+  this._setAsComplete()
+}
+
+FinalFrame.prototype.isFinishedRolling = function () {
+  return this.rollThree !== undefined
+}
+
+FinalFrame.prototype._isBonus = function () {
+  if (this.isSpare || this.isStrike) return true
+}
